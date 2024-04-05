5204ad83ec85edaa1498791367afd0363a8e0ee5
Airblow <143172177+Airblow@users.noreply.github.com>
2024-04-05 13:06:46 +0200

    Add project files.

---
+++ KonstruktorerUpg1/Bilregister.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonstruktorerUpg1
+{
+    class Bilregister
+    {
+        private string brand;
+        private string carModel;
+        private string carColor;
+        private int productionYear;
+        private int distanceTraveled;
+
+        public Bilregister(string b, string cM, string cC, int pY, int dT)
+        {
+            brand = b;
+            carModel = cM;
+            carColor = cC;
+            productionYear = pY;
+            distanceTraveled = dT;
+
+        }
+
+        public void Output(string b, string cM, string cC, int pY, int dT)
+        {
+            Console.WriteLine(b);
+            Console.WriteLine(cM);
+            Console.WriteLine(cC);
+            Console.WriteLine(pY);
+            Console.WriteLine(dT);
+        }
+    }
+}
---
+++ KonstruktorerUpg1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Xml;
+
+namespace KonstruktorerUpg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool failsafe = false;
+
+            while (failsafe != true)
+            {
+                try
+                {
+                    Console.WriteLine("Skapa ett bilregister");
+                    Console.WriteLine("Skriv in bilens märke");
+                    string brand = Console.ReadLine();
+                    Console.Clear();
+
+                    Console.WriteLine("Skriv in bilens modell");
+                    string carModel = Console.ReadLine();
+                    Console.Clear();
+
+                    Console.WriteLine("Skriv in bilens färg");
+                    string carColor = Console.ReadLine();
+                    Console.Clear();
+
+                    Console.WriteLine("Vilket år producerades bilen");
+                    int productionYear = int.Parse(Console.ReadLine());
+                    Console.Clear();
+
+                    Console.WriteLine("Hur långt har bilen körts? (I KM)");
+                    int distanceTraveled = int.Parse(Console.ReadLine());
+                    Console.Clear();
+
+
+                    Bilregister b1 = new Bilregister(brand, carModel, carColor, productionYear, distanceTraveled);
+
+                    b1.Output(brand, carModel, carColor, productionYear, distanceTraveled);
+                }
+                catch
+                {
+                    Console.WriteLine("Något gick fel, försök igen.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+            }
+
+        }
+
+        
+    }
+
+}
